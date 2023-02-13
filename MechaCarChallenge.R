867ac0c159d23b59edfeccaa25a0d7cefd3a2141
Mark <mark@obrimail.com>
2023-02-13 15:38:50 -0700

    mod 16 update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,14 @@
 library(dplyr)
 readfile <- read.csv("MechaCar_mpg.csv")
-lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars)
-summary(lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars))
+library(tidyverse)
+lm(mpg ~ vehicle_length + vehicle_weight	+ spoiler_angle +	ground_clearance + AWD,data=readfile)
+summary(lm(mpg ~ vehicle_length + vehicle_weight	+ spoiler_angle +	ground_clearance + AWD,data=readfile))
+
+
+suspension_data <- read.csv("Suspension_coil.csv")
+#summarize(PSI)
+mean(suspension_data$PSI)
+#summarize(gap2010, mean_life_exp=weighted.mean(life_exp, population, na.rm=TRUE))
+
+summarize(suspension_data,column1=mean(PSI), column)
+
